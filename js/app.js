30d866e03b5587086052357ba28edca93aa4b8c5
Joshua Loschen <josh.loschen@gmail.com>
2017-12-17 11:52:00 -0600

    Got collisions to be detected Got player to restart at bottom when reaching the water. Prevented character from going off screen

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var tileHeight = 80;
+var tileHeight = 81;
 var tileWidth = 101;
-var upMovement = -80;
-var leftMovement = -101;
+var upMovement = -1 * tileHeight;
+var leftMovement = -1 * tileWidth;
 var score = 0;
 var laneSpeed = [100, 150, 200];
 var spriteDictionary = {
@@ -11,80 +11,64 @@ var spriteDictionary = {
     pinkGirl:"images/char-pink-girl.png",
     princess:"images/char-princess-girl.png",
 };
-// Enemies our player must avoid
-var Enemy = function(lane) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    //this.speed = Math.random() * 200 + 50;
-    this.speed = laneSpeed[lane - 1];
-    
-    this.x = 0;
-    //const lane = Math.floor(Math.random() * 3) + 1;
-    //console.log('creating enemit' + lane);
-    this.y = lane * tileHeight - 20;
-    //console.log('creating enemit' + lane + ' ' + this.y);
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    //console.log('app enemy constructor');
-};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += (this.speed * dt);
-    if(this.x > 500){
-        this.x = - 10;
-//        const lane = Math.floor(Math.random() * 3) + 1;
-//        this.y = lane * tileHeight - 20;
+// Enemies our player must avoid
+class Enemy{
+    constructor(lane){
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+        //this.speed = Math.random() * 200 + 50;
+        this.speed = laneSpeed[lane - 1];
+
+        this.x = 0;
+        
+        //const lane = Math.floor(Math.random() * 3) + 1;
+        this.y = lane * tileHeight - 20;
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+//        this.sprite = 'images/dot.PNG';
     }
     
-    //console.log('app update');
-};
+    update(dt){
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x += (this.speed * dt);
+        if(this.x > 500){
+            this.x = - 10;
+        }
+    }
+    
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.draw
+    }
+    
+    isInHitBox(player){
+        if(Math.abs(player.x - this.x) < 70 && Math.abs(player.y - this.y) < 20){
+            player.reset();
+        }
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    //this.x = 3 * tileSize;
-    //this.y = 1 * tileSize;
-    
-    //river at -20
-    //top    lane at 60y
-    //middle lane at 140 y;
-    //bottom lane at 220 y;
-    
-    //column 0:0
-    //column 1:
-    //column 2:
-    //column 3:
-    //column 4:
-    
-    //this.x = 404;
-    //this.y = 380;
-    //console.log('render enemy at '+ this.x + ' ' + this.y);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
     constructor(characterType){
-        //console.log('player ctor');
         this.x = 200;
         this.y = 390;
         this.Ymovement = 0;
         this.Xmovement = 0;
         this.inWater = false;
-        
+        this.halfWidth = 20;
+        this.halfHeight = 20;
         this.sprite = spriteDictionary[characterType];
-        //console.log(this.sprite);
     }
     
     render(){
-        //console.log('x:' + this.x + ' y:' + this.y);
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     
@@ -97,17 +81,27 @@ class Player {
         if(this.Ymovement !== 0){
             this.y += this.Ymovement;
             this.Ymovement = 0;
-            
             //if they won
             if(this.y < 50 && !this.inWater){
                 score++;
                 var numKeys = document.getElementById("score");
                 numKeys.innerHTML = score;
-                
                 this.inWater = true;
-                setTimeout(function(){
-                    movePlayerBack(this);}
-                           , 2000);
+                
+                //using arrow function to lexically bind 'this'
+                //https://stackoverflow.com/questions/2130241/pass-correct-this-context-to-settimeout-callback
+                setTimeout(() => {
+                    this.reset();
+                } , 600);
+            }
+        }
+        
+        //check for collisions
+        for(const enemy of allEnemies){
+            var differenceX = Math.abs(this.x - enemy.x);
+            var differenceY = Math.abs(this.y - enemy.y);
+            if(differenceX < 70 && differenceY < 20){
+                player.reset();
             }
         }
     }
@@ -115,36 +109,37 @@ class Player {
     handleInput(direction){
         switch(direction){
             case 'left':
-                this.Xmovement = leftMovement;
+                if(this.x > 0) 
+                    this.Xmovement = leftMovement;    
                 break;
             case 'up':
-                this.Ymovement = upMovement;
+                if(this.y > 0)
+                    this.Ymovement = upMovement;
                 break;
             case 'right':
-                this.Xmovement = tileWidth;
+                if(this.x < 400) 
+                    this.Xmovement = tileWidth;
                 break;
             case 'down':
-                this.Ymovement = tileHeight;
+                if(this.y < 350)
+                    this.Ymovement = tileHeight;
                 break;
         }
     }
+    
+    reset(){
+        this.x = 200;
+        this.y = 390;
+        this.inWater = false;
+    }
 }
-
-function movePlayerBack(player){
-    console.log('resetting');
-    player.x = 200;
-    player.y = 390;
-    player.inWater = false;
-}
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [/*new Enemy(), new Enemy()*/, new Enemy(1), new Enemy(2), new Enemy(3)];
+var allEnemies = [/*new Enemy(), new Enemy(),*/ new Enemy(1), new Enemy(2), new Enemy(3)];
 
 var player = new Player('boy');
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
